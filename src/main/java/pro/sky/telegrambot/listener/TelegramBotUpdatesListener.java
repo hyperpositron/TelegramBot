de185e907ff587b07e530a3d1d4f28b60e226f72
hyperpositron <hyperpositron@gmail.com>
2023-08-14 13:34:30 +0200

    correction 14.08.2023 - 13:34

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,19 +2,33 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.time.LocalDateTime.parse;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final Logger LOG = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final Pattern PATTERN = Pattern.compile("(\\d{1,2}\\.\\d{1,2}\\.\\d{4} \\d{1,2}:\\d{2}) ([А-яA-z\\d,\\s!?:@.]+)");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d.M.yyyy HH:mm");
 
     @Autowired
     private TelegramBot telegramBot;
@@ -27,14 +41,42 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            System.out.println("Халлоу");
-            update.message().text();
-            update.message().chat().id();
+            LOG.info("Processing update: {}", update);
+            Long id = update.message().chat().id();
+            Message message = update.message();
+            String text = message.text();
+            LocalDateTime dateTime;
+            if (update.message() != null && text != null) {
+                Matcher matcher = PATTERN.matcher(text);
+                if (text.equals("/start")) {
+                    SendMessage sendMessage = new SendMessage(
+                            id,
+                            "Для планирования задачи отправте её в формате:\\n*01.01.2022 20:00 Сделать домашнюю работу*"
+                    );
+                    sendMessage.parseMode(ParseMode.Markdown);/*Оформление*/
+                    SendResponse sendResponse = telegramBot.execute(sendMessage); /*Если много символов будет 400 ,что бы понять*/
+                    if (!sendResponse.isOk()) {
+                        LOG.error("SendMessage was failed due to: " + sendResponse.description());
+                    }
+                } else if (matcher.matches() && (dateTime = parse(matcher.group(1))) != null) {
+                    String notification = matcher.group(2);
+
+                } else {
+                    /*пишем, что не понимаем юзера*/
+                }
+            }
 
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    @Nullable
+    private LocalDateTime parse(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, DATE_TIME_FORMATTER);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
 }
