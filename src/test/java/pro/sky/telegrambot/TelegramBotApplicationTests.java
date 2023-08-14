4c1bdafede3caf9f190aa538f75c29df42c5d965
hyperpositron <hyperpositron@gmail.com>
2023-08-14 18:50:04 +0200

    commit test 14.08.2023 - 18:49

--- src/test/java/pro/sky/telegrambot/TelegramBotApplicationTests.java
+++ src/test/java/pro/sky/telegrambot/TelegramBotApplicationTests.java
@@ -1,13 +1,69 @@
 package pro.sky.telegrambot;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import pro.sky.telegrambot.listener.TelegramBotUpdatesListener;
+import pro.sky.telegrambot.service.NotificationTaskService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 @SpringBootTest
 class TelegramBotApplicationTests {
+    @Mock
+    private TelegramBot telegramBot;
+    @Mock
+    private NotificationTaskService notificationTaskService;
+    @InjectMocks
+    private TelegramBotUpdatesListener telegramBotUpdatesListener;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void handStartTest() throws URISyntaxException, IOException {
+        String json = Files.readString(
+                Paths.get(TelegramBotUpdatesListenerTest.class.getResource("text_update.json").toURI()));
+        Update update = getUpdate(json, "/start");
+        telegramBotUpdatesListener.process(Collections.singletonList(update));
+
+        ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
+        Mockito.verify(telegramBot).execute(argumentCaptor.capture());
+        SendMessage actual = argumentCaptor.getValue();
+
+        Assertions.assertThat(actual.getParameters().get("chat_id")).isEqualTo(123L);
+        Assertions.assertThat(actual.getParameters().get("text")).isEqualTo(
+                "Для планирования задачи отправте её в формате:\n*01.01.2022 20:00 Сделать домашнюю работу*");
+        Assertions.assertThat(actual.getParameters().get("parse_mode"))
+                .isEqualTo(ParseMode.Markdown.name());
+
+    }
+
+    @Test
+    public void handleInvalidMessage() throws URISyntaxException, IOException {
+        String json = Files.readString(
+                Paths.get(TelegramBotUpdatesListenerTest.class.getResource("text_update.json").toURI()));
+        Update update = getUpdate(json, "hello world");
+        telegramBotUpdatesListener.process(Collections.singletonList(update));
+
+        ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
+        Mockito.verify(telegramBot).execute(argumentCaptor.capture());
+        SendMessage actual = argumentCaptor.getValue();
+
+        Assertions.assertThat(actual.getParameters().get("chat_id")).isEqualTo(123L);
+        Assertions.assertThat(actual.getParameters().get("text")).isEqualTo(
+                "Некорректный формат задачи для планирования! Корректный формат: 01.01.2022 20:00 Сделать домашнюю работу");
+
+    }
 
 }
