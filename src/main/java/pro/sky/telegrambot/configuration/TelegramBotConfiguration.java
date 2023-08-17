79c8df2e8880f6e333653a6cd10c8f4aa55fa891
hyperpositron <hyperpositron@gmail.com>
2023-08-17 14:49:23 +0200

    17.08.2023 - 14:49

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -9,11 +9,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TelegramBotConfiguration {
 
-    @Value("${telegram.bot.token}")
-    private String token;
-
     @Bean
-    public TelegramBot telegramBot() {
+    public TelegramBot telegramBot(@Value("${telegram.bot.token}") String token) {
         TelegramBot bot = new TelegramBot(token);
         bot.execute(new DeleteMyCommands());
         return bot;
